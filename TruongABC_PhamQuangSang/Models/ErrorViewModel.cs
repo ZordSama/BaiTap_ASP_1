6666925e8f8304b9ab6c886f7e3e7400cc1fceea
Zord <quangsang.red@gmail.com>
2024-02-19 21:38:58 +0700

    init

---
+++ TruongABC_PhamQuangSang/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TruongABC_PhamQuangSang.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
